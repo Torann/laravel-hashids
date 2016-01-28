942f6786905de97be7ccfd849f1ada16e83a8726
Daniel Stainback <torann@gmail.com>
2016-01-28 14:51:34 -0500

    - autoload with PSR-4 - update to v1.0.6 of HashID - add support for lumen

--- src/Torann/Hashids/Facade.php
+++ src/Torann/Hashids/Facade.php
@@ -1,14 +1,18 @@
-<?php namespace Torann\Hashids;
+<?php
+
+namespace Torann\Hashids;
 
 use Illuminate\Support\Facades\Facade as IlluminateFacade;
 
-class Facade extends IlluminateFacade {
-
+class Facade extends IlluminateFacade
+{
     /**
      * Get the registered component.
      *
      * @return object
      */
-    protected static function getFacadeAccessor() { return 'hashids'; }
-
+    protected static function getFacadeAccessor()
+    {
+        return 'hashids';
+    }
 }
--- src/Torann/Hashids/HashidsServiceProvider.php
+++ src/Torann/Hashids/HashidsServiceProvider.php
@@ -4,48 +4,60 @@ use Hashids\Hashids;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\AliasLoader;
 
-class HashidsServiceProvider extends ServiceProvider {
+class HashidsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->isLumen() === false) {
+            $this->publishes([
+                __DIR__ . '/../../config/hashids.php' => config_path('hashids.php'),
+            ]);
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-        $this->publishes([
-            __DIR__.'/../../config/hashids.php' => config_path('hashids.php'),
-        ]);
+            // Add 'Assets' facade alias
+            AliasLoader::getInstance()->alias('Hashids', 'Torann\Hashids\Facade');
+        }
+    }
 
-		// Add 'Assets' facade alias
-		AliasLoader::getInstance()->alias('Hashids', 'Torann\Hashids\Facade');
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('hashids', function ($app) {
+            $config = $app->config->get('hashids');
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		// Bind 'hashids' shared component to the IoC container
-		$this->app->singleton('hashids', function($app)
-		{
-			// Read settings from config file
-            $config = $app->config->get('hashids', array());
+            return new Hashids(
+                array_get($config, 'salt'),
+                array_get($config, 'length', 0),
+                array_get($config, 'alphabet')
+            );
+        });
+    }
 
-           	return new Hashids($config['salt'], $config['length'], $config['alphabet']);
-		});
-	}
+    /**
+     * Check if package is running under Lumen app
+     *
+     * @return bool
+     */
+    protected function isLumen()
+    {
+        return str_contains($this->app->version(), 'Lumen') === true;
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['hashids'];
+    }
 }
